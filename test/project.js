3b13c3f66b70f18ecbf57f804a6995c4d4344d99
ajlopez <webmaster@ajlopez.com>
2014-08-10 08:20:26 -0300

    Project first test

---
+++ test/project.js
@@ -0,0 +1,23 @@
+
+var sl = require('..');
+
+var list = [
+    { id: 1, name: 'Adam', age: 800 },
+    { id: 2, name: 'Eve', age: 600 },
+    { id: 3, name: 'Abel', age: 500 },
+    { id: 4, name: 'Caine', age: 400 }
+];
+
+exports['project name'] = function (test) {
+    var result = sl.project(list, ['name']);
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, list.length);
+    
+    for (var k = 0; k < list.length; k++) {
+        test.equal(result[k].name, list[k].name);
+        test.equal(Object.keys(result[k]).length, 1);
+    }
+}
+
