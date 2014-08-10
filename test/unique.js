b68f3d6dfea4b1e785dacad5f12b0a079dcce27f
ajlopez <webmaster@ajlopez.com>
2014-08-10 08:53:38 -0300

    Unique first tests

---
+++ test/unique.js
@@ -0,0 +1,33 @@
+
+var sl = require('..');
+
+var list = [
+    { id: 1, name: 'Adam', age: 800, gender: 'man' },
+    { id: 2, name: 'Eve', age: 600, gender: 'woman' },
+    { id: 3, name: 'Abel', age: 500, gender: 'man' },
+    { id: 4, name: 'Caine', age: 400, gender: 'man' }
+];
+
+exports['unique id'] = function (test) {
+    var result = sl.unique(list, 'id');
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, list.length);
+    
+    for (var k = 0; k < result.length; k++)
+        test.equal(result[k].id, list[k].id);
+}
+
+exports['unique genre'] = function (test) {
+    var result = sl.unique(list, 'gender');
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 2);
+    
+    test.equal(result[0].gender, 'man');
+    test.equal(result[0].name, 'Adam');
+    test.equal(result[1].gender, 'woman');
+    test.equal(result[1].name, 'Eve');
+}
