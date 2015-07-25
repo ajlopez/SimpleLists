69fd5dcf89ec4dba297a814e13441a73b4f285ba
ajlopez <webmaster@ajlopez.com>
2015-07-25 11:17:14 -0300

    All predicate

---
+++ test/all.js
@@ -0,0 +1,25 @@
+
+var sl = require('..');
+
+var list = [
+    { id: 1, name: 'Adam', age: 800, gender: 'man' },
+    { id: 2, name: 'Eve', age: 600, gender: 'woman' },
+    { id: 3, name: 'Abel', age: 500, gender: 'man' },
+    { id: 4, name: 'Caine', age: 400, gender: 'man' }
+];
+
+exports['all with age'] = function (test) {
+    test.ok(sl.all(list, "age"));
+}
+
+exports['all with age and name'] = function (test) {
+    test.ok(sl.all(list, [ "age", "name" ]));
+}
+
+exports['all with age, name and parents'] = function (test) {
+    test.ok(!sl.all(list, [ "age", "name", "parents" ]));
+}
+
+exports['all with positive age'] = function (test) {
+    test.ok(sl.all(list, function (obj) { return obj.age > 0; }));
+}
