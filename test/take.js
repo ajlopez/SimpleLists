52631848ba002957a57d035cde5031526b5850dc
ajlopez <webmaster@ajlopez.com>
2016-07-23 11:55:26 -0300

    Take n elements

---
+++ test/take.js
@@ -0,0 +1,19 @@
+
+var sl = require('..');
+
+exports['take three elements'] = function (test) {
+    var values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    
+    var taken = sl.take(values, 3);
+    
+    test.ok(taken);
+    test.ok(Array.isArray(taken));
+    test.equal(taken.length, 3);
+    
+    test.equal(taken[0], 0);
+    test.equal(taken[1], 1);
+    test.equal(taken[2], 2);
+    
+    test.equal(values.length, 10);
+};
+
