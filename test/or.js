33af7d5b66a3ed00790330915bbbbbedcdab14de
ajlopez <webmaster@ajlopez.com>
2017-12-01 11:06:54 -0300

    Filter or WIP

--- test/or.js
+++ test/or.js
@@ -19,3 +19,13 @@ exports['filter name is Adam or name is Eve'] = function (test) {
 	test.ok(sl.exist(result, { name: 'Eve' }));
 }
 
+exports['filter name is Adam or name is Eve and gender is man'] = function (test) {
+    var result = sl.filter(list, { $or: [ { name: 'Adam' }, { name: 'Eve' } ], gender: 'man' });
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 1);
+	
+	test.ok(sl.exist(result, { name: 'Adam' }));
+}
+
