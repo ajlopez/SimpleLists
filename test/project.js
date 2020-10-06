1675c80f4703015e80b94990128048006dd776ba
ajlopez <webmaster@ajlopez.com>
2020-10-06 07:41:52 -0300

    Refactor project tests

--- test/project.js
+++ test/project.js
@@ -1,7 +1,7 @@
 
-var sl = require('..');
+const sl = require('..');
 
-var list = [
+const list = [
     { id: 1, name: 'Adam', age: 800 },
     { id: 2, name: 'Eve', age: 600 },
     { id: 3, name: 'Abel', age: 500 },
@@ -9,38 +9,38 @@ var list = [
 ];
 
 exports['project name'] = function (test) {
-    var result = sl.project(list, ['name']);
+    const result = sl.project(list, ['name']);
     
     test.ok(result);
     test.ok(Array.isArray(result));
     test.equal(result.length, list.length);
     
-    for (var k = 0; k < list.length; k++) {
+    for (let k = 0; k < list.length; k++) {
         test.equal(result[k].name, list[k].name);
         test.equal(Object.keys(result[k]).length, 1);
     }
 }
 
 exports['project only name'] = function (test) {
-    var result = sl.project(list, 'name');
+    const result = sl.project(list, 'name');
     
     test.ok(result);
     test.ok(Array.isArray(result));
     test.equal(result.length, list.length);
     
-    for (var k = 0; k < list.length; k++) {
+    for (let k = 0; k < list.length; k++) {
         test.equal(result[k].name, list[k].name);
         test.equal(Object.keys(result[k]).length, 1);
     }
 }
-exports['project id and name'] = function (test) {    var result = sl.project(list, ['id', 'name']);        test.ok(result);    test.ok(Array.isArray(result));    test.equal(result.length, list.length);        for (var k = 0; k < list.length; k++) {        test.equal(result[k].id, list[k].id);        test.equal(result[k].name, list[k].name);        test.equal(Object.keys(result[k]).length, 2);    }}exports['project id and name with rename'] = function (test) {
-    var result = sl.project(list, { id: 'id2', name: 'name2' });
+exports['project id and name'] = function (test) {    const result = sl.project(list, ['id', 'name']);        test.ok(result);    test.ok(Array.isArray(result));    test.equal(result.length, list.length);        for (let k = 0; k < list.length; k++) {        test.equal(result[k].id, list[k].id);        test.equal(result[k].name, list[k].name);        test.equal(Object.keys(result[k]).length, 2);    }}exports['project id and name with rename'] = function (test) {
+    const result = sl.project(list, { id: 'id2', name: 'name2' });
     
     test.ok(result);
     test.ok(Array.isArray(result));
     test.equal(result.length, list.length);
     
-    for (var k = 0; k < list.length; k++) {
+    for (let k = 0; k < list.length; k++) {
         test.equal(result[k].id2, list[k].id);
         test.equal(result[k].name2, list[k].name);
         test.equal(Object.keys(result[k]).length, 2);
