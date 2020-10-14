86da80fd310b9c3d1b336c0aef66ade0c2443666
ajlopez <webmaster@ajlopez.com>
2020-10-14 11:47:09 -0300

    Sort tests refactor

--- test/sort.js
+++ test/sort.js
@@ -1,7 +1,7 @@
 
-var sl = require('..');
+const sl = require('..');
 
-var list = [
+const list = [
     { id: 1, name: 'Adam', age: 800 },
     { id: 2, name: 'Eve', age: 600 },
     { id: 3, name: 'Abel', age: 500 },
@@ -9,23 +9,23 @@ var list = [
 ];
 
 exports['sort by name'] = function (test) {
-    var result = sl.sort(list, 'name');
+    const result = sl.sort(list, 'name');
     
     test.ok(result);
     test.ok(Array.isArray(result));
     test.equal(result.length, list.length);
-    for (var k = 1; k < list.length; k++)
+    for (let k = 1; k < list.length; k++)
         test.ok(result[k - 1].name <= result[k].name);
 }
-exports['sort by age'] = function (test) {    var result = sl.sort(list, 'age');        test.ok(result);    test.ok(Array.isArray(result));    test.equal(result.length, list.length);    for (var k = 1; k < list.length; k++)        test.ok(result[k - 1].age <= result[k].age);}exports['sort by height and age'] = function (test) {
-    var result = sl.sort(list, ['height', 'age']);
+exports['sort by age'] = function (test) {    const result = sl.sort(list, 'age');        test.ok(result);    test.ok(Array.isArray(result));    test.equal(result.length, list.length);    for (let k = 1; k < list.length; k++)        test.ok(result[k - 1].age <= result[k].age);}exports['sort by height and age'] = function (test) {
+    const result = sl.sort(list, ['height', 'age']);
     
     test.ok(result);
     test.ok(Array.isArray(result));
     test.equal(result.length, list.length);
 
-    for (var k = 1; k < list.length; k++)
+    for (let k = 1; k < list.length; k++)
         test.ok(result[k - 1].age <= result[k].age);
 }
 
-exports['sort by descending name'] = function (test) {    var result = sl.sort(list, 'name', true);        test.ok(result);    test.ok(Array.isArray(result));    test.equal(result.length, list.length);    for (var k = 1; k < list.length; k++)        test.ok(result[k - 1].name >= result[k].name);}
+exports['sort by descending name'] = function (test) {    const result = sl.sort(list, 'name', true);        test.ok(result);    test.ok(Array.isArray(result));    test.equal(result.length, list.length);    for (let k = 1; k < list.length; k++)        test.ok(result[k - 1].name >= result[k].name);}
